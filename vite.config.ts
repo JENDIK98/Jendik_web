4197fe80530cc3714368efdcbac661882382c71c
Jan Stanek <jan.stanek.official@gmail.com>
2025-09-11 22:51:18 -0400

    Fix asset names and ensure all paths are correct

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,15 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/main.js',
+        chunkFileNames: 'assets/[name].js',
+        assetFileNames: 'assets/main.css'
+      }
+    }
+  },
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
